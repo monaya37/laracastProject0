85b02fbaefe30372d8c9e9da2bef5b50cd74c603
Khaira Bah <Monayaa1637@gmail.com>
2023-09-18 06:06:16 -0800

    passing Post as and object

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,19 +1,48 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 
 class Post
 {
 
+public $title;
+public $excerpt;
+public $date;
+public $body;
+public $slug;
+    
+public function __construct($title, $excerpt,$date, $body, $slug){
+    $this->title = $title;
+    $this->excerpt = $excerpt;
+    $this->date = $date;
+    $this->body = $body;
+    $this->slug = $slug;
+}
+
+
     public static function all()
     {
-
+        //use the File facade, the "files()" method takes a directory
+        //it returns a type of "symfony" aka some data about that directory
+        //in this example it returns info about the files stored in posts directory (my-#th-post)
         $files = File::files(resource_path("posts/"));
 
-        return array_map(fn($file)=> $file->getContents(),$files);
+
+        //in the array_map funtion we are looping over y
+        //each item is passed to x
+        // array_map(funtion(x) {return x.stuff(); } ,y)
+        //فاهمة كل شي يكون ليش يرجع شي "ستف" من الاكس للاكس يعني ليه ما يرجع شي من الواي للاكس؟
+        // الجواب: هو لما يرجع ما يرجعها للاكس هو يكبظ عنصر من الواي ويعطيه للاكس بعدين ممكن اني ناخذ معلومات ذاك العنصر اللي هو اكس
+
+        //loop over $files and getContents of each item and pass it to $file
+        return array_map(fn ($file) => $file->getContents(), $files);
     }
+
+
+    //understood 100%
     public static function find($slug)
     {
         $path = resource_path("posts/{$slug}.html");
@@ -23,6 +52,6 @@ class Post
         }
 
         // get the content of the path, cache it, then retrun it
-        return cache()->remember("posts.{$slug}", 1200, fn()=> file_get_contents($path));
+        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\File;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +17,31 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
+
+    // YamlFrontMatter::parseFile($path);
+    //the document has all data in "my-forth-post" including the metadata and the body
+    // YamlFrontMatter::parseFile(
+    //    $document =  resource_path('posts/my-forth-post.html')
+    // );
+
+    $files = File::files(resource_path("posts/"));
+    $posts = [];
+
+    foreach($files as $file){
+        $document = YamlFrontMatter::parseFile($file);
+
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }
+
+
+
+    // $posts = Post::all();
 
     return view('posts', [
         'posts'=> $posts
