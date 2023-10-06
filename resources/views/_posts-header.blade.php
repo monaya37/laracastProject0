b386392d46f16b39279d2acf0ce5c337834c41d1
Khaira Bah <Monayaa1637@gmail.com>
2023-10-06 06:10:10 -0800

    more work on dropdown

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -15,39 +15,38 @@
 
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
-            <div x-data="{ show: false }" @click.away="show = false">
 
-                <button @click="show = !show"
-                    class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
-                    {{ isset($currentCategory) ? $currentCategory->name : 'categories' }}
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                        </g>
-                    </svg>
 
-                </button>
+            <x-dropdown>
+                <x-slot name="trigger">
 
-                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl" style="display:none">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
 
+                        {{ isset($currentCategory) ? $currentCategory->name : 'categories' }}
+
+                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;"
+                            width="22" height="22" viewBox="0 0 22 22">
+                            <g fill="none" fill-rule="evenodd">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                                </path>
+                                <path fill="#222"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                            </g>
+                        </svg>
+
+                    </button>
+                </x-slot>
                     <a href="/"
                         class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white
-                         focus:text-white">All</a>
+             focus:text-white">All</a>
 
                     @foreach ($categories as $category)
                         <a href="/categories/{{ $category->slug }}"
                             class="block text-left px-3 text-sm leading-6 
-                            hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
-                            {{ isset($currentCategory) && $currentCategory->id == $category->id ? 'bg-blue-500 text-white' : '' }}"
-                            
-                            >{{ $category->name }}</a>
+                hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
+                {{ isset($currentCategory) && $currentCategory->id == $category->id ? 'bg-blue-500 text-white' : '' }}">{{ $category->name }}</a>
                     @endforeach
-                </div>
-            </div>
+            </x-dropdown>
 
         </div>
 
