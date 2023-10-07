ee07c1e0b2b62673b8e48b19139af2323772ff07
Khaira Bah <Monayaa1637@gmail.com>
2023-10-06 21:31:58 -0800

    cleaning up

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -5,7 +5,7 @@
 {{$trigger}}
    </div>
 
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl" style="display:none">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl overflow-auto max-h-52" style="display:none">
         {{$slot}}
     </div>
 </div>
